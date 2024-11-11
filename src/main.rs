47958d3dfa6ad98182da00a566b08c7a26bf32ac
Zoe Soutter <zoe@soutter.com>
2024-11-11 14:17:51 +0000

    Add inbox system

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,141 @@
-use std::{io::{Read, Write}, net::{TcpListener, TcpStream}, time::Duration};
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+    time::Duration,
+};
 
 use regex::Regex;
 
 fn main() {
-    let mut file = std::fs::OpenOptions::new().append(true).create(true).open("./email_log").unwrap();
+    let mut file = std::fs::OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("./email_log")
+        .unwrap();
     let listener = TcpListener::bind("0.0.0.0:25").unwrap();
 
     for mut incoming in listener.incoming().flatten() {
-        incoming.write_all(b"220 zoe.soutter.com ESMTP Postfix\r\n").unwrap();
-        file.write_all(b"C: 220 zoe.soutter.com ESMTP Postfix\r\n").unwrap();
-        let data = read_timeout(&mut incoming);
-        println!("Intro: {}", String::from_utf8_lossy(&data));
-        write!(file, "S: {}\r\n", String::from_utf8_lossy(&data)).unwrap();
-        incoming.write_all(b"250 Hello ").unwrap();
-        incoming.write_all(&data[5..(data.len() - 2)]).unwrap();
-        incoming.write_all(b", I am glad to meet you\r\n").unwrap();
-        file.write_all(b"C: 250 Hello ").unwrap();
-        file.write_all(&data[5..(data.len() - 2)]).unwrap();
-        file.write_all(b", I am glad to meet you\r\n").unwrap();
-        loop {
-            let data = read_timeout(&mut incoming);
-            if data == b"DATA\r\n" {
-                file.write_all(b"S: DATA\r\n").unwrap();
-                break;
-            }
-            incoming.write_all(b"250 Ok\r\n").unwrap();
-            file.write_all(b"C: 250 Ok\r\n").unwrap();
-            println!("S: {}", String::from_utf8_lossy(&data));
-        write!(file, "S: {}\r\n", String::from_utf8_lossy(&data)).unwrap();
-        }
-        incoming.write_all(b"354 End data with <CR><LF>.<CR><LF>\r\n").unwrap();
-        file.write_all(b"C: 354 End data with <CR><LF>.<CR><LF>\r\n").unwrap();
-        let data = read_timeout(&mut incoming);
-
+        let Ok(email) = parse_smtp_packet(&mut incoming) else {continue;};
         let x = regex::bytes::Regex::new(r"(https?://)?[-a-zA-Z0-9%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)").unwrap();
-        let urls = x.find_iter(&data);
-
-        println!("{}", String::from_utf8_lossy(&data));
-        write!(file, "S: {}\r\n", String::from_utf8_lossy(&data)).unwrap();
-        incoming.write_all(b"250 Ok: queued as 12345\r\n").unwrap();
-        file.write_all(b"C: 250 Ok: queued as 12345\r\n").unwrap();
-        let data = read_timeout(&mut incoming);
-        println!("{}", String::from_utf8_lossy(&data));
-        write!(file, "{}\r\n", String::from_utf8_lossy(&data)).unwrap();
-        incoming.write_all(b"221 Bye\r\n").unwrap();
-        file.write_all(b"C: 221 Bye\r\n").unwrap();
-        incoming.shutdown(std::net::Shutdown::Both).unwrap();
-        println!("CONNECTION CLOSED");
-        write!(file, "CONNECTION CLOSED\r\n").unwrap();
-
+        let urls = x.find_iter(&email.body);
         for url in urls {
-            println!("URL FOUND: {}", String::from_utf8_lossy(url.as_bytes()));
-            write!(file, "URL FOUND: {}\r\n", String::from_utf8_lossy(url.as_bytes())).unwrap();
+            println!("URL found: {}", String::from_utf8_lossy(url.as_bytes()));
         }
     }
 }
 fn read_timeout(stream: &mut TcpStream) -> Vec<u8> {
     let mut buffer = Vec::new();
-    stream.set_read_timeout(Some(Duration::from_millis(100))).unwrap();
+    stream
+        .set_read_timeout(Some(Duration::from_millis(100)))
+        .unwrap();
     if let Err(err) = stream.read_to_end(&mut buffer) {
-        if let std::io::ErrorKind::WouldBlock = err.kind() {
-
-        } else {
+        if std::io::ErrorKind::WouldBlock != err.kind() {
             println!("{err:?}");
         }
     }
     buffer
 }
+fn parse_smtp_packet(stream: &mut TcpStream) -> std::io::Result<IncomingEmail> {
+    stream.write_all(b"220 zoe.soutter.com ESMTP Postfix\r\n")?;
+    let data = read_timeout(stream);
+    if data.len() <= 5 {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "Client returned too short of a response",
+        ));
+    };
+    let name = &data[5..(data.len() - 2)];
+    stream.write_all(
+        format!(
+            "250 Hello {}, I am glad to meet you\r\n",
+            String::from_utf8_lossy(&name)
+        )
+        .as_bytes(),
+    )?;
+    let mut recipients = Vec::new();
+    let mut sender = None;
+    loop {
+        let data = read_timeout(stream);
+        if let Ok(val) = prse::try_parse!(String::from_utf8_lossy(&data), "MAIL FROM:<{}>\r\n") {
+            let val: String = val;
+            if let Some((username, domain)) = val.split_once("@") {
+                sender = Some(EmailAddress {
+                    username: username.to_string(),
+                    domain: domain.to_string(),
+                })
+            }
+            stream.write_all(b"250 Ok\r\n")?;
+        } else if let Ok(val) = prse::try_parse!(String::from_utf8_lossy(&data), "RCPT TO:<{}>\r\n")
+        {
+            let val: String = val;
+            if let Some((username, domain)) = val.split_once("@") {
+                recipients.push(EmailAddress {
+                    username: username.to_string(),
+                    domain: domain.to_string(),
+                })
+            }
+            stream.write_all(b"250 Ok\r\n")?;
+        } else if &data == b"DATA\r\n" {
+            let data = read_timeout(stream);
+            stream.write_all(b"354 End data with <CR><LF>.<CR><LF>\r\n")?;
+            break;
+        } else {
+            stream.write_all(b"250 Ok\r\n")?;
+        }
+    }
+    let Some(sender) = sender else {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "Client addressed no sender",
+        ));
+    };
+    if recipients.len() == 0 {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "Client addressed no recipients",
+        ));
+    }
+    let body_data = read_timeout(stream);
+    stream.write_all(b"250 Ok: Queued as\r\n")?;
+    for recipient in recipients.iter().cloned() {
+        if recipient.is_safe() && recipient.domain == "zoe.soutter.com" && sender.is_safe() {
+            let time = chrono::Local::now().format("%Y-%m-%d-%H:%M:%S-").to_string();
+            let path = format!("./{}@{}/{}@{}-{}.email", recipient.username, recipient.domain, time, sender.username, sender.domain);
+            let _ = std::fs::create_dir(format!("./{}@{}", recipient.username, recipient.domain));
+            if let Ok(mut file) = std::fs::OpenOptions::new().write(true).create_new(true).open(&path) {
+                file.write_all(&body_data)?;
+            } else {
+                println!("Failed to create email file for {}", path);
+            }
+        }
+    }
+    let response = read_timeout(stream);
+    if response == b"QUIT\r\n" {
+        stream.write_all(b"221 Bye\r\n")?;
+        stream.shutdown(std::net::Shutdown::Both)?;
+        return Ok(IncomingEmail {
+            to_addresses: recipients,
+            from_address: sender,
+            body: body_data,
+        });
+    }
+
+    todo!()
+}
+#[derive(Clone, Debug)]
 struct EmailAddress {
     username: String,
     domain: String,
 }
+impl EmailAddress {
+    fn is_safe(&self) -> bool {
+        self.username.chars().all(|chr| chr.is_alphanumeric() || chr == '+' || chr == '-' || chr == '_' || chr == '.')
+        && self.domain.chars().all(|chr| chr.is_alphanumeric() || chr == '+' || chr == '-' || chr == '_' || chr == '.')
+    }
+}
+#[derive(Clone, Debug)]
 struct IncomingEmail {
-    to_address: EmailAddress,
+    to_addresses: Vec<EmailAddress>,
     from_address: EmailAddress,
+    body: Vec<u8>,
 }
