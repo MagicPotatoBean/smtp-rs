49077458e0f6b445985d7af76b1dc7f9381bdac6
Zoe Soutter <zoe@soutter.com>
2024-12-09 13:05:32 +0000

    Allow any recipient domain

--- src/main.rs
+++ src/main.rs
@@ -145,7 +145,7 @@ fn parse_smtp_packet(stream: &mut TcpStream) -> std::io::Result<IncomingEmail> {
         .collect();
     stream.write_all(b"250 Ok: Queued as\r\n")?;
     for recipient in recipients.iter().cloned() {
-        if recipient.is_safe() && recipient.domain == "zoe.soutter.com" && sender.is_safe() {
+        if recipient.is_safe() && sender.is_safe() {
             let time = chrono::Local::now().format("%Y.%m.%d-%H:%M:%S").to_string();
             let path = format!(
                 "./inboxes/{}@{}/{}@{}-{}.email",
